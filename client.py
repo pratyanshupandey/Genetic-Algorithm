9913ce0d7c377c05ce5bd2274c7246944f24c00a
pratyanshupandey <pratyan2001@gmail.com>
2021-02-28 02:06:23 +0530

    experimenting with data index 0

--- client.py
+++ client.py
@@ -23,7 +23,11 @@ def get_errors(id, vector):
     for i in vector: assert 0<=abs(i)<=10
     assert len(vector) == MAX_DEG
 
-    return json.loads(send_request(id, vector, 'geterrors'))
+    ret_val = json.loads(send_request(id, vector, 'geterrors'))
+    log = open("log.txt", "a")
+    log.write(str(vector) + " : " + str(ret_val) + "\n")
+    log.close()
+    return ret_val
 
 def get_overfit_vector(id):
     return json.loads(send_request(id, [0], 'getoverfit'))
@@ -32,4 +36,4 @@ def get_overfit_vector(id):
 # if __name__ == "__main__":
 #     overfit_vector = [0.0, -1.45799022e-12, -2.28980078e-13,  4.62010753e-11, -1.75214813e-10, -1.83669770e-15,  8.52944060e-16,  2.29423303e-05, -2.04721003e-06, -1.59792834e-08,  9.98214034e-10]
 #     print(get_errors('SOql4uavXyMdC9BTYktZDz152sPIhQLm6ucxoy2ujxmqb8o7E1', overfit_vector))
-#     # print(get_overfit_vector('SOql4uavXyMdC9BTYktZDz152sPIhQLm6ucxoy2ujxmqb8o7E1'))
+#     print(get_overfit_vector('SOql4uavXyMdC9BTYktZDz152sPIhQLm6ucxoy2ujxmqb8o7E1'))
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from client import get_errors, MAX_DEG
-import numpy as np
+from client import get_errors
+# import numpy as np
 ID = 'SOql4uavXyMdC9BTYktZDz152sPIhQLm6ucxoy2ujxmqb8o7E1'
-
-def create_population():
-    pass
-
+OVERFIT_VECTOR = [0.0, -1.457990220064754e-12, -2.2898007842769645e-13, 4.620107525277624e-11, -1.7521481289918844e-10, -1.8366976965696096e-15, 8.529440604118815e-16, 2.2942330256117977e-05, -2.0472100298772093e-06, -1.597928341587757e-08, 9.982140340891233e-10]
+#
+# def create_population():
+#     pass
+#
+#
+# vectors = create_population()
+# for i in range(10):
+#     selection(vectors)
+#     crossover(vectors)
+#     mutation(vectors)
+vector = OVERFIT_VECTOR
+for i in range(11):
+    vector[0] = -10 + i/10
+    print(vector , " : " , get_errors(ID,vector))
