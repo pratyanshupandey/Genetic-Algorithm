8c1ea6242db3c3dffcc3a5151e54ec8e4918d52b
pratyanshupandey <pratyan2001@gmail.com>
2021-03-08 20:10:40 +0530

    test

--- geterrors.py
+++ geterrors.py
@@ -3,11 +3,11 @@ from client2 import get_errors
 ID = 'SOql4uavXyMdC9BTYktZDz152sPIhQLm6ucxoy2ujxmqb8o7E1'
 
 
-with open("25.json", "r") as file:
-    print("loading old population")
-    vectors = json.load(file)
-
-assert len(vectors) == 10
-for vector in vectors:
-    assert len(vector) == 11
-    print(get_errors(ID, vector))
+# with open("25.json", "r") as file:
+#     print("loading old population")
+#     vectors = json.load(file)
+#
+# assert len(vectors) == 10
+# for vector in vectors:
+assert len(vector) == 11
+print(get_errors(ID, vector))
