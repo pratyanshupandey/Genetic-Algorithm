c678f8aea6949a638ec1275a4d0d38b1cdccdea9
pratyanshupandey <pratyan2001@gmail.com>
2021-03-16 20:13:48 +0530

    genrations files created

--- Deliverables/generations/copyscript.py
+++ Deliverables/generations/copyscript.py
@@ -1,13 +1,13 @@
 tempelate_file = open("ready_for_gen_file_old23.txt", "r")
 vector_file = open("../../NEW2/gen_file_old3.txt", "r")
-write_file = open("generations_1.txt", "a")
+write_file = open("generations_10.txt", "a")
 while True:
     line = tempelate_file.readline()
     if line == "":
         break
     write_file.write(line)
 
-upto = 33198
+upto = 35986
 for i in range(upto):
     line = vector_file.readline()
     write_file.write(line)
